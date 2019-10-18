fcad70857c924e91013aa00729250b4aeef8eaeb
gibsonbailey <gibson.bailey@live.com>
2019-10-17 18:41:14 -0700

    Tiny change made by Nic.

--- image_upload.py
+++ image_upload.py
@@ -3,4 +3,9 @@ import flask
 from werkzeug.utils import secure_filename
 
 UPLOAD_FOLDER = '/home/pi/led_project/media/'
-jj:wq
+ALLOWED_EXTENSIONS = {''}
+
+app = Flask(__name__)
+app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
+
+
