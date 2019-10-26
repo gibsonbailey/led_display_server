0b5ac64df5180cfd4ded79ced741f3247a58dd79
gibsonbailey <gibson.bailey@live.com>
2019-10-26 16:51:12 -0700

    Added text input form.

--- image_upload.py
+++ image_upload.py
@@ -4,6 +4,7 @@ from flask import Flask, flash, request, redirect, url_for, send_from_directory
 from werkzeug.utils import secure_filename
 
 latest_upload_filename = ''
+latest_upload_text = ''
 
 UPLOAD_FOLDER = '/home/pi/led_project/media'
 ALLOWED_EXTENSIONS = set(['txt', 'pdf', 'png', 'jpg', 'jpeg', 'gif'])
@@ -21,10 +22,19 @@ def uploaded_file(filename):
 @app.route('/', methods=['GET', 'POST'])
 def upload_file():
     global latest_upload_filename
+    global latest_upload_text
+
     print("UF", file=sys.stderr)
     if request.method == 'POST':
 # check if the post request has the file part
         print("POST", file=sys.stderr)
+
+        text = request.form.get('message')
+        if text:
+            latest_upload_text = text
+            print(latest_upload_text, file=sys.stderr)
+            # Send to LED Board and Redirect
+
         if 'file' not in request.files:
             print("No File", file=sys.stderr)
             flash('No file part')
@@ -43,24 +53,28 @@ def upload_file():
             url = url_for('uploaded_file', filename=filename)
             print('url for output', url, file=sys.stderr)
             return redirect(request.url)
-    return '''
-    <!doctype
-    html>
-    <title>Upload
-    new
-    File</title>
-    <h1>Upload
-    new
-    File</h1>
-    <form
-    method=post
-    enctype=multipart/form-data>
-    <input
-    type=file
-    name=file>
-    <input
-    type=submit
-    value=Upload>
+
+    response = '''
+    <!doctype html>
+    <head>
+    <link rel="icon" href="/uploads/Git-Logo-Black.png">
+    </head>
+
+    <title>Upload new File</title>
+
+    <h1>Upload new File</h1>
+
+    <form method=post enctype=multipart/form-data>
+        <input type=text name=message>
+        <input type=file name=file>
+        <input type=submit value=Upload>
     </form>
-    <img src="/uploads/{}" style="width: 100%;">
-    '''.format(latest_upload_filename)
+    ''' 
+    if latest_upload_text:
+        header_tag = '<h4>{}</h4>'.format(latest_upload_text)
+        response += header_tag
+    if latest_upload_filename:
+       image_tag = '<img src="/uploads/{}" style="width: 60%;">'.format(latest_upload_filename)
+       response += image_tag
+     
+    return response
